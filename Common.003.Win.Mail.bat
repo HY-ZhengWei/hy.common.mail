f3660e9a9225cc105ae70ff6620bf4f6c8501cc5
HY-ZhengWei <hy.zhengwei@qq.com>
2018-03-07 12:06:50 +0800

    2.0.3

--- Common.003.Win.Mail.bat
+++ Common.003.Win.Mail.bat
@@ -2,9 +2,19 @@
 
 cd .\bin
 
+
 jar cvfm hy.common.mail.jar MANIFEST.MF META-INF org
 
 copy hy.common.mail.jar ..
 del /q hy.common.mail.jar
 cd ..
 
+
+
+
+
+cd .\src
+jar cvfm hy.common.mail-sources.jar MANIFEST.MF META-INF org 
+copy hy.common.mail-sources.jar ..
+del /q hy.common.mail-sources.jar
+cd ..
--- Common.004.Mac.Mail.sh
+++ Common.004.Mac.Mail.sh
@@ -2,9 +2,19 @@
 
 cd ./bin
 
+
 jar cvfm hy.common.mail.jar MANIFEST.MF META-INF org
 
 cp hy.common.mail.jar ..
 rm hy.common.mail.jar
 cd ..
 
+
+
+
+
+cd ./src
+jar cvfm hy.common.mail-sources.jar MANIFEST.MF META-INF org 
+cp hy.common.mail-sources.jar ..
+rm hy.common.mail-sources.jar
+cd ..
